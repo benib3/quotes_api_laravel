3df1c3f0bda5ffcca8d5966358cc4af57503bdf2
Beni Bambur <beni.bambur@outlook.com>
2026-03-10 10:25:19 +0100

    remove unused migrations for password resets and failed jobs; add spotify_current_plan migration

--- database/migrations/2026_03_10_091628_create_spotify_current_plan_table.php
+++ database/migrations/2026_03_10_091628_create_spotify_current_plan_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('spotify_current_plan', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->float('amount')->unique();
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('spotify_current_plan');
     }
 };
